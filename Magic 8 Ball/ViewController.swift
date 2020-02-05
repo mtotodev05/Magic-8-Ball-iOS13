0c95ad3e1c2208754f44c548f2179a0208cc7bb4
mtotodev05 <mtotodev05@gmail.com>
2020-02-05 10:11:48 +0300

    Complete magic ball task

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Declare image array with all the image literals.
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBOutlet weak var magicBallView: UIImageView!
+    
+    @IBAction func onProvideAnswer(_ sender: Any) {
+        // Give an random ball count.
+        magicBallView.image = ballArray[Int.random(in: 0...ballArray.count - 1)]
+    }
 }
 
